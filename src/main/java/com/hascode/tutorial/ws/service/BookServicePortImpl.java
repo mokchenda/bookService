b6df7d84d2ead9ef71228bf8d7610225cd1d1683
mokchend <mokchend@yahoo.com>
2014-12-26 16:29:17 +0100

    make it better

---
+++ src/main/java/com/hascode/tutorial/ws/service/BookServicePortImpl.java
@@ -0,0 +1,38 @@
+package com.hascode.tutorial.ws.service;
+
+import java.util.GregorianCalendar;
+
+import javax.jws.WebService;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+
+import com.hascode.tutorial.ws.service.BookServicePortType;
+import com.hascode.tutorial.ws.service.BookServiceRequestType;
+import com.hascode.tutorial.ws.service.BookServiceResponseType;
+import com.hascode.tutorial.ws.service.BookType;
+
+
+
+@WebService(endpointInterface = "com.hascode.tutorial.ws.service.BookServicePortType")
+public class BookServicePortImpl implements BookServicePortType {
+	//@Override
+	public BookServiceResponseType fetchBooks(
+			BookServiceRequestType bookServiceRequest) {
+		final BookServiceResponseType response = new BookServiceResponseType();
+		for (int i = 0; i < bookServiceRequest.getLimit(); i++) {
+			final BookType book = new BookType();
+			book.setAuthor("Elvis " + i);
+			try {
+				book.setPublished(DatatypeFactory.newInstance()
+						.newXMLGregorianCalendar(
+								new GregorianCalendar(2011, 8, 14)));
+			} catch (DatatypeConfigurationException e) {
+			}
+			book.setTitle("Programming Java Edition #" + i);
+			response.getBook().add(book);
+		}
+		return response;
+	}
+
+
+}
---
+++ target/heroku/app/.profile.d/jvmcommon.sh
@@ -0,0 +1,16 @@
+limit=$(ulimit -u)
+case $limit in
+256)   # 1X Dyno
+  heap=384
+;;
+512)   # 2X Dyno
+  heap=768
+;;
+32768) # PX Dyno
+  heap=6144
+;;
+*)
+  heap=384
+;;
+esac
+export JAVA_TOOL_OPTIONS="-Xmx${heap}m $JAVA_TOOL_OPTIONS -Djava.rmi.server.useCodebaseOnly=true"
