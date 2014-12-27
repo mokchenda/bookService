70b27bbf35552fcf69aa456da9b7c8bc67caf7c6
mokchend <mokchend@yahoo.com>
2014-12-27 09:06:40 +0100

    all

--- target/heroku/app/target/generated/src/main/java/com/hascode/tutorial/ws/service/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.hascode.com/bookService/1.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.hascode.tutorial.ws.service;
